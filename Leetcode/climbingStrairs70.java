a18b93b563b3ad4231c51dedfbc28f03150db104
Priya Deo <priyadeo347@gmail.com>
2026-09-02 13:20:11 +0530

    code for leetcode problem 70

---
+++ Leetcode/climbingStrairs70.java
@@ -0,0 +1,20 @@
+public class climbingStrairs70 {
+    public int climbStairs(int n) {
+        if(n==1){
+            return 1;
+        }
+        int one = 1;
+        int two = 2;
+        
+        for (int i=3; i<=n; i++){
+            int steps = one + two;
+            one = two;
+            two = steps;
+        }
+        
+        return two;
+    }
+}
+     {
+    
+}

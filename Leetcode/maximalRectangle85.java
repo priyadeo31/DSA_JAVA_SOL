348b9a1bb7ab921fb7be550c7eb60a6e85b0bdfd
Priya Deo <priyadeo347@gmail.com>
2026-09-03 11:04:57 +0530

    code for leetcode problem 85

---
+++ Leetcode/maximalRectangle85.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+public class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix.length == 0) return 0;
+ int m = matrix.length, n = matrix[0].length;
+ int[] heights = new int[n];
+ int maxArea = 0;
+ for (int i = 0; i < m; i++) {
+ for (int j = 0; j < n; j++) {
+ if (matrix[i][j] == '1') heights[j]++;
+ else heights[j] = 0;
+ }
+ maxArea = Math.max(maxArea, largestRectangleArea(heights));
+ }
+ return maxArea;
+    }
+    private static int largestRectangleArea(int[] heights) {
+ Stack<Integer> stack = new Stack<>();
+ int maxArea = 0;
+ for (int i = 0; i <= heights.length; i++) {
+ int h = (i == heights.length) ? 0 : heights[i];
+ while (!stack.isEmpty() && h < heights[stack.peek()]){
+     int height = heights[stack.pop()];
+ int width = stack.isEmpty() ? i : i - stack.peek()-1;
+  maxArea = Math.max(maxArea, height * width);
+ }
+ stack.push(i);
+ }
+ return maxArea;
+}} {
+    
+}
